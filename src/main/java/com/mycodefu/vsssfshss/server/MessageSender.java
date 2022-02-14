30e1e9fadad6d4119999c2a705df0e60c6879124
Luke Thompson <endeavour9@gmail.com>
2022-02-14 20:05:24 +1100

    Switch to using channel instead of ChannelID. Store name in attribute.

--- src/main/java/com/mycodefu/vsssfshss/server/MessageSender.java
+++ src/main/java/com/mycodefu/vsssfshss/server/MessageSender.java
@@ -1,9 +1,10 @@
 package com.mycodefu.vsssfshss.server;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelId;
 
 public interface MessageSender {
-    void sendMessage(ChannelId channel, ByteBuf message);
-    void broadcast(ByteBuf message, ChannelId... excludeChannelIds);
+    void sendMessage(Channel channel, ByteBuf message);
+    void broadcast(ByteBuf message, Channel... excludeChannels);
 }
