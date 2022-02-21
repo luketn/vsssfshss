ace90b73acb9f86d59e5c9364ebf745d266424d8
Luke Thompson <endeavour9@gmail.com>
2022-02-21 16:35:06 +1100

    Switch to Quarkus based on maven template: mvn io.quarkus.platform:quarkus-maven-plugin:2.7.1.Final:create -DprojectGroupId=com.mycodefu -DprojectArtifactId=quarkus-websockets -Dextensions="websockets" -DnoExamples

--- src/main/java/com/mycodefu/vsssfshss/server/MessageSender.java
+++
@@ -1,8 +0,0 @@
-package com.mycodefu.vsssfshss.server;
-
-import io.netty.channel.Channel;
-
-public interface MessageSender {
-    void sendMessage(Channel channel, String message);
-    void broadcast(String message, Channel... excludeChannels);
-}
