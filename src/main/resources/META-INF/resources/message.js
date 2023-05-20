1dee586396e213da927a314470712cc052e9ba94
Jett Thompson <jett.thompson.59@gmail.com>
2023-05-21 07:07:13 +1000

    add the ability for the website to format links

---
+++ src/main/resources/META-INF/resources/message.js
@@ -0,0 +1,29 @@
+const renderers = [
+    [/(https?:\/\/[^\s]+)/g, renderLink],
+]
+
+function renderLink(url) {
+    return `<a href="${url}" target="_blank">${url}</a>`
+}
+
+function escapeHTML(unsafeText) {
+    return unsafeText.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+}
+
+function generateHTMLForMessage(chatMessage) {
+    return `
+        <div class="message">
+            <div class="message__author">${escapeHTML(chatMessage.name)}:</div>
+            <div class="message__content">
+                ${renderMessage(escapeHTML(chatMessage.message))}
+            </div>
+        </div>
+    `
+}
+
+function renderMessage(message) {
+    for (const [regex, renderer] of renderers) {
+        message = message.replace(regex, renderer)
+    }
+    return message
+}
